a8a2f341d5cfe3500f347898762a2e5018b83597
cferdinandi <chris@gomakethings.com>
2018-04-26 11:51:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GMT EDD Add Product Names to Emails
+Adds a tag for adding purchased product names to EDD automated emails.
---
+++ gmt-edd-add-product-names-to-emails.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: GMT EDD Add Product Names to Emails
+ * Plugin URI: https://github.com/cferdinandi/gmt-edd-add-product-names-to-emails/
+ * GitHub Plugin URI: https://github.com/cferdinandi/gmt-edd-add-product-names-to-emails/
+ * Description: Add WP Rest API hooks into Easy Digital Downloads.
+ * Version: 0.1.0
+ * Author: Chris Ferdinandi
+ * Author URI: http://gomakethings.com
+ * License: GPLv3
+ */
+
